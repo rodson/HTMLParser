249e173090ff89a068879dda154246257c77a298
rodsonluo <rodsonluo@tencent.com>
2018-03-08 10:01:17 +0800

    add rollup

--- src/parser.js
+++ src/parser.js
@@ -3,7 +3,21 @@
  */
 
 export const parseHTML = (html, options) => {
+  const stack = [];
+  const expectHTML = true;
+  const isUnaryTag =
+    'area,base,br,col,embed,frame,hr,img,input,isindex,keygen,' +
+    'link,meta,param,source,track,wbr';
+  const canBeLeftOpenTag =
+    'colgroup,dd,dt,li,options,p,td,tfoot,th,thead,tr,source';
+  let index = 0;
+  let last;
+  let lastTag;
+
   while (html) {
+    last = html;
+
+    // not in plain text element
     // comment
     // start tag
     // end tag
---
+++ src/utils.js
@@ -0,0 +1,9 @@
+// create a map based on the input string delimited by comma
+export function makeMap(str) {
+  const map = Object.create(null);
+  const list = str.split(',');
+  list.forEach(item => {
+    map[item] = true;
+  });
+  return val => map[val];
+}
