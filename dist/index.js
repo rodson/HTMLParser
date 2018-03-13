0821838ea96fb1910d7691cf5f2682ffbe34c1da
rodsonluo <rodsonluo@gmail.com>
2018-03-13 20:04:20 +0800

    update

--- dist/index.js
+++ dist/index.js
@@ -1,9 +1,194 @@
 (function (global, factory) {
-	typeof exports === 'object' && typeof module !== 'undefined' ? factory() :
+	typeof exports === 'object' && typeof module !== 'undefined' ? module.exports = factory() :
 	typeof define === 'function' && define.amd ? define(factory) :
-	(factory());
+	(global.bundle = factory());
 }(this, (function () { 'use strict';
 
-console.log('hello');
+/**
+ * parse html string to tokens
+ */
+const attrRegex = /^\s*([^\s"'<>\/=]+)(?:\s*(=)\s*(?:"([^"]*)"+|'([^']*)'+|([^\s"'=<>`]+)))?/;
+const ncname = '[a-zA-Z_][\\w\\-\\.]*';
+const qname = `((?:${ncname}\\:)?${ncname})`;
+const startTagOpenRegex = new RegExp(`^<${qname}`);
+const startTagCloseRegex = /^\s*(\/?)>/;
+const endTagRegex = new RegExp(`^<\\/${qname}>`);
+const commentRegex = /^<!\--/;
+
+function tokenize(html) {
+  let tokens = [];
+  while (html) {
+
+    let textEnd = html.indexOf('<');
+    if (textEnd === 0) {
+      // comment
+      if (commentRegex.test(html)) {
+        const commentEnd = html.indexOf('-->');
+        if (commentEnd > 0) {
+          tokens.push({
+            type: 'comment',
+            value: html.substring(4, commentEnd)
+          });
+          advance(commentEnd + 3);
+          continue;
+        }
+      }
+      // start tag
+      const startTagOpen = html.match(startTagOpenRegex);
+      if (startTagOpen) {
+        tokens.push({
+          type: 'startTagOpen',
+          value: startTagOpen[1]
+        });
+        advance(startTagOpen[0].length);
+        let startTagClose;
+        let attr;
+        while (!(startTagClose = html.match(startTagCloseRegex)) &&
+          (attr = html.match(attrRegex))
+        ) {
+          advance(attr[0].length);
+          tokens.push({
+            type: 'attrName',
+            value: attr[1]
+          });
+          tokens.push({
+            type: 'attrValue',
+            value: attr[3] || attr[4] || attr[5] || ''
+          });
+        }
+        if (startTagClose) {
+          advance(startTagClose[0].length);
+          tokens.push({
+            type: 'startTagClose',
+            value: '>'
+          });
+          if (startTagClose[1]) {
+            tokens.push({
+              type: 'endTag',
+              value: startTagOpen[1]
+            });
+          }
+        }
+        continue;
+      }
+      // end tag
+      const endTag = html.match(endTagRegex);
+      if (endTag) {
+        advance(endTag[0].length);
+        tokens.push({
+          type: 'endTag',
+          value: endTag[1]
+        });
+        continue;
+      }
+    }
+
+    // text content
+    if (textEnd > 0) {
+      const text = html.substring(0, textEnd);
+      advance(textEnd);
+      tokens.push({
+        type: 'text',
+        value: text
+      });
+    }
+
+    if (textEnd < 0) {
+      tokens.push({
+        type: 'text',
+        value: html
+      });
+      html = '';
+    }
+  }
+
+  //============ helper function ============//
+  function advance(n) {
+    html = html.substring(n);
+  }
+  //========== end helper function ==========//
+
+  return tokens;
+}
+
+/**
+ * parse tokens to AST
+ */
+function parse(tokens = []) {
+  const root = {
+    type: 'root',
+    children: []
+  };
+  let current = 0;
+  let tagStack = [];
+  tagStack.push(root);
+  while (current < tokens.length) {
+    let token = tokens[current];
+    current++;
+    if (token.type === 'startTagOpen') {
+      const tagName = token.value;
+      const attrs = {};
+      token = tokens[current];
+      while (token.type !== 'startTagClose') {
+        if (token.type === 'attrName') {
+          attrs[token.value] = tokens[++current].value;
+        }
+        current++;
+        token = tokens[current];
+      }
+      const element = createASTElement({
+        tag: tagName,
+        attrs
+      });
+      tagStack[tagStack.length - 1].children.push(element);
+      tagStack.push(element);
+      current++;
+    } else if (token.type === 'endTag') {
+      tagStack.pop();
+      current++;
+    } else if (token.type === 'text') {
+      const text = createASTElement({
+        tag: 'text'
+      });
+      tagStack[tagStack.length - 1].children.push(text);
+      current++;
+    }
+  }
+  return root;
+}
+
+function createASTElement({
+  tag,
+  attrs = {}
+}) {
+  return {
+    tag,
+    attrs,
+    children: []
+  };
+}
+
+function traverse(ast, visitor) {
+  function traverseArray(array, parent) {
+    array.forEach(child => {
+      traverseNode(child, parent);
+    });
+  }
+
+  function traverseNode(node, parent) {
+    visitor(node, parent);
+    traverseArray(node.children, node);
+  }
+
+  traverseNode(ast, null);
+}
+
+var index = {
+  tokenize,
+  parse,
+  traverse
+}
+
+return index;
 
 })));
--- test.js
+++ test.js
@@ -1,7 +1,6 @@
 import assert from 'assert';
 import tokenize from './src/tokenize';
 import parse from './src/parse';
-import transform from './src/transform';
 import util from 'util';
 
 describe('tokenizer', () => {
@@ -77,25 +76,5 @@ describe('parser', () => {
         children: []
       }]
     }]
-  });
-});
-
-describe('transformer', () => {
-  it('should transform current AST to a new AST', () => {
-    const ast = {
-      type: 'root',
-      children: [{
-        tag: 'h1',
-        attrs: {
-          class: 'title'
-        },
-        children: [{
-          tag: 'text',
-          attrs: {},
-          children: []
-        }]
-      }]
-    };
-    assert.deepEqual(transform(ast), {});
   });
 });
